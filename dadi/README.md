fbff531eb8aa93b70bcee7009ff224deab8b18bc
AlessandroSottosanti <asottosanti2@gmail.com>
2024-11-04 16:08:09 +0100

    feat: dadi

--- dadi/README.md
+++ dadi/README.md
@@ -1,10 +1,15 @@
 ### Esercizio Dadi
 
 ## Dati
-
+- Numero giocatore da 1 a 6 numeri random
+- Numero computer da 1 a 6 numeri random
 
 ## Esecuzione Logica
-
+Determinare il vincitore della partita a dadi in base al numero random ottenuto:
+- Se il giocatore avrà un numero più alto di quello del computer, si avrà una vittoria;
+- Se il giocatore avrà un numero più basso di quello del computer, si avrà una sconfitta.
 
 ## Output
-
+In base all'esito avremo un messaggio in console di:
+- VITTORIA;
+- SCONFITTA.
--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,44 +1,22 @@
-// generare 10 numeri random e salvarli in un array
 
-const numbers = [];
 
-for(let i = 0; i < 10; i++) {
-    const randomNum = Math.floor(Math.random() * 100) + 1;
-    numbers.push(randomNum);
+// DATI
+const playerNum = Math.floor(Math.random() *6) + 1;
+const cmpNum = Math.floor(Math.random() *6) + 1;
+
+
+console.log("Player: ", playerNum);
+console.log("Computer: ", cmpNum);
+
+console.log("");
+
+// ESECUZIONE LOGICA & OUTPUT
+if(playerNum > cmpNum) {
+    console.log("You Won!");
 }
-
-console.log(numbers);
-
-for(let i = 0; i < numbers.length; i++) {
-    const curItem = numbers[i];
-    console.log(`index ${i}, valore ${curItem}`);
-
-    if (curItem % 2 === 0) {
-        console.log("Pari")
-    }
-    else{
-        console.log("Dispari");
-    }
-
+else if(playerNum == cmpNum){
+    console.log("Tie.");
 }
-
-console.log("")
-
-console.log("stampa elementi dell'array con index dispari:")
-
-
-for(let i = 0; i < numbers.length; i++) {
-    // i è l'index/posizione
-
-    const curItem = numbers[i];
-    // curItem è il valore a quell'index
-
-    
-    // Stampa index dispari
-    if(i % 2 !== 0) {
-        console.log(`posizione dispari : index ${i}, valore ${curItem}`);
-    }
-    else {
-        console.log(`index ${i}, valore ${curItem}`);
-    }
+else{
+    console.log("Wasted.");
 }
