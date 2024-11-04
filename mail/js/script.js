5fb403894c6082486af630fe7eaab0d32640b025
AlessandroSottosanti <asottosanti2@gmail.com>
2024-11-04 15:50:11 +0100

    feat: mail ex.

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,10 +7,26 @@ const authorizedMails = [
     'paperino@gmail.com',
     'topolino@gmail.com'
 ]
-console.log(authorizedMails);
-
+authorized = false;
 userMail = prompt("Inserire la popria email di accesso");
+
 console.log(userMail);
+console.log(authorizedMails);
 
 // ESECUZIONE LOGICA
 
+for(let i = 0; i < authorizedMails.length; i++){
+    currentEmail = authorizedMails[i];
+    if(currentEmail === userMail){
+        authorized = true;
+    }
+}
+
+// OUTPUT
+
+if(authorized === true){
+    console.log("Accesso eseguito con successo!");
+}
+else{
+    console.log("Email non autorizzata all'accesso.");
+}
